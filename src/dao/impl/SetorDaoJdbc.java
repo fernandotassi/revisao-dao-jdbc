98dab2969c994b750737a3a6d6e9567d27c5ed84
fernandotassi <apropriate3@gmail.com>
2025-10-24 14:10:00 -0300

    ArigoDaoJdbc e SetorDaoJdbc

---
+++ src/dao/impl/SetorDaoJdbc.java
@@ -0,0 +1,45 @@
+package dao.impl;
+
+import java.util.List;
+
+import dao.SetorDao;
+import entities.Setor;
+
+public class SetorDaoJdbc implements SetorDao
+{
+	@Override
+	public void inseri(Setor set) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualiza(Setor set) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleta(Integer id) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Setor find(Integer id) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Setor> findall() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
